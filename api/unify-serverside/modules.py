1c2bbf8e7e51b04692143c2442f41fc44db37628
callum-else <callum.else@gmail.com>
2020-02-07 20:47:07 +0000

    Moved DB connection code to utility module

--- api/unify-serverside/modules.py
+++ api/unify-serverside/modules.py
@@ -1,8 +1,9 @@
 import falcon
 import json
-import pymysql.cursors
+import pymysql
+from .utility import db_connect
 
-class test_db_connection:
+class DBConnectionTest:
 """
 Class for testing the connection to the Database from the scripts.
 """
@@ -10,24 +11,13 @@ Class for testing the connection to the Database from the scripts.
     def on_get(self, req, resp):
 
         output = {
-            'resp':None
+            'db_connected':False
         }
 
-        try:
-            connection = pymysql.connect (
-                host='localhost',
-                port=3306,
-                user='unify',
-                password='V8oU!LkuYz',
-                db='unify',
-                charset='utf8mb4',
-                cursorclass=pymysql.cursors.DictCursor
-            )
+        connection = db_connect()
 
-            if connection.open:
-                output['resp'] = "Connection Established."
-            else:
-                output['resp'] = "Connection Failed."
+        try:
+            output['db_connected'] = connection.open
 
         finally:
             connection.close()
